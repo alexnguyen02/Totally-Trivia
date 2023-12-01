ee7afc88a2e7c7609e795478b853b140ce47265e
DanielKaloshi <daniel.kaloshi@mail.utoronto.ca>
2023-12-01 02:06:08 +0100

    Added Welcome, Main, and Account views

---
+++ src/view/AccountView.java
@@ -0,0 +1,75 @@
+package view;
+
+import interface_adaptors.ViewManagerModel;
+import view.SelectColourView;
+import view.LogoutView;
+import view.DeleteView;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AccountView {
+
+    public String viewName = "account";
+    private final JButton changeColour;
+    private final JButton logOut;
+
+    private final JButton deleteAccount;
+
+    private final ViewManagerModel viewManagerModel;
+
+    private final SelectColourView selectColourView;
+
+    private final LogoutView logoutView;
+
+    private final DeleteView deleteView;
+
+    public AccountView(ViewManagerModel viewManagerModel, SelectColourView selectColourView, LogoutView logoutView, DeleteView deleteView) {
+
+        this.viewManagerModel = viewManagerModel;
+        this.selectColourView = selectColourView;
+        this.logoutView = logoutView;
+        this.deleteView = deleteView;
+
+
+        JPanel buttons = new JPanel();
+        changeColour = new JButton("Change Background Colour");
+        buttons.add(changeColour);
+        logOut = new JButton("Log Out");
+        buttons.add(logOut);
+        deleteAccount = new JButton("Delete Account");
+        buttons.add(deleteAccount);
+
+        changeColour.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                if (evt.getSource().equals(changeColour)) {
+                    viewManagerModel.setActiveView(selectColourView.viewName);
+                    viewManagerModel.firePropertyChanged();
+                }
+            }
+        });
+
+        logOut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                if (evt.getSource().equals(logOut)) {
+                    viewManagerModel.setActiveView(logoutView.viewName);
+                    viewManagerModel.firePropertyChanged();
+                }
+            }
+        });
+
+        deleteAccount.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                if (evt.getSource().equals(deleteAccount)) {
+                    viewManagerModel.setActiveView(deleteView.viewName);
+                    viewManagerModel.firePropertyChanged();
+                }
+            }
+        });
+
+    }
+}
---
+++ src/view/MainScreenView.java
@@ -0,0 +1,57 @@
+package view;
+
+import interface_adaptors.ViewManagerModel;
+import view.SelectModeView;
+import view.AccountView;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainScreenView {
+
+    public String viewName = "main screen";
+    private final JButton playGame;
+    private final JButton account;
+
+    private final ViewManagerModel viewManagerModel;
+
+    private final SelectModeView selectModeView;
+
+    private final AccountView accountView;
+
+    public MainScreenView(ViewManagerModel viewManagerModel, SelectModeView selectModeView, AccountView accountView) {
+
+        this.viewManagerModel = viewManagerModel;
+        this.selectModeView = selectModeView;
+        this.accountView = accountView;
+
+
+        JPanel buttons = new JPanel();
+        playGame = new JButton("Play Game");
+        buttons.add(playGame);
+        account = new JButton("Account");
+        buttons.add(account);
+
+        playGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                if (evt.getSource().equals(playGame)) {
+                    viewManagerModel.setActiveView(selectModeView.viewName);
+                    viewManagerModel.firePropertyChanged();
+                }
+            }
+        });
+
+        account.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                if (evt.getSource().equals(account)) {
+                    viewManagerModel.setActiveView(accountView.viewName);
+                    viewManagerModel.firePropertyChanged();
+                }
+            }
+        });
+
+    }
+}
