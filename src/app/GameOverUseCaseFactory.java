20373029a9167cf399809949ea6766b2d0dd3bbe
Kate Levey <144384708+lemoncake206@users.noreply.github.com>
2023-12-03 21:21:48 -0500

    Updated GameOverUseCaseFactory.java

--- src/app/GameOverUseCaseFactory.java
+++ src/app/GameOverUseCaseFactory.java
@@ -8,6 +8,7 @@ import interface_adaptors.game_over.GameOverPresenter;
 import use_case.game_over.GameOverInputBoundary;
 import use_case.game_over.GameOverInteractor;
 import use_case.game_over.GameOverOutputBoundary;
+import use_case.game_over.GameOverUserDataAccessInterface;
 import view.GameOverView;
 import entity.User;
 
@@ -19,16 +20,17 @@ public class GameOverUseCaseFactory {
     public static GameOverView create(
             ViewManagerModel viewManagerModel,
             GameOverViewModel gameOverViewModel,
-            FileUserDataAccessObject fileUserDataAccessObject, User user) {
+            GameOverUserDataAccessInterface fileUserDataAccessObject, User user) {
         GameOverController gameOverController = createGameOverController(viewManagerModel, fileUserDataAccessObject, user);
         return new GameOverView(gameOverViewModel, gameOverController);
     }
 
     private static GameOverController createGameOverController(ViewManagerModel viewManagerModel,
-                                                            FileUserDataAccessObject fileUserDataAccessObject, User user) {
+                                                            GameOverUserDataAccessInterface fileUserDataAccessObject, User user) {
         GameOverOutputBoundary gameOverOutputBoundary = new GameOverPresenter(viewManagerModel);
         GameOverInputBoundary questionInteractor = new GameOverInteractor(gameOverOutputBoundary, fileUserDataAccessObject, user);
         return new GameOverController(questionInteractor);
     }
 
 }
+
