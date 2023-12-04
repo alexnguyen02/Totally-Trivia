c5994a5f7583a7de1472c55b8149a83328c1ca31
kate levey <katelevey@kates-MacBook-Pro.local>
2023-12-03 23:37:44 -0500

    Fixed some bugs

--- src/use_case/delete/DeleteUserDataAccessInterface.java
+++ src/use_case/delete/DeleteUserDataAccessInterface.java
@@ -1,6 +1,10 @@
 package use_case.delete;
 
 
+import entity.User;
+
 public interface DeleteUserDataAccessInterface {
+
+    void save(User user);
     String delete(String s);
 }
--- test/view/DeleteViewTest.java
+++ test/view/DeleteViewTest.java
@@ -1,13 +1,22 @@
 package view;
 
+import app.DeleteUseCaseFactory;
+import data_access.InMemoryUserDataAccessObject;
+import entity.CommonUserFactory;
+import entity.User;
+import entity.UserFactory;
+import interface_adaptors.ViewManagerModel;
 import interface_adaptors.delete.DeleteController;
 import interface_adaptors.delete.DeleteViewModel;
 import org.junit.jupiter.api.Test;
+import use_case.delete.DeleteUserDataAccessInterface;
 import view.DeleteView;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,8 +24,17 @@ public class DeleteViewTest {
 
     @Test
     public void testViewName() {
-        DeleteController deleteController = new DeleteController(null);
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
         DeleteViewModel deleteViewModel = new DeleteViewModel();
+        UserFactory userFactory = new CommonUserFactory();
+        DeleteUserDataAccessInterface userDataAccessObject = new InMemoryUserDataAccessObject();
+        User user = userFactory.create("Furiosa", "RememberMe", LocalDateTime.now(), 50, "Red");
+        User for_repository = userFactory.create("","", LocalDateTime.now(), 0, "White");
+        for_repository.copyUser(user);
+        userDataAccessObject.save(for_repository);
+        String s = user.getName();
+        DeleteController deleteController = DeleteUseCaseFactory.createUserDeleteUseCase(viewManagerModel, deleteViewModel, user, userDataAccessObject);
+
         DeleteView deleteView = new DeleteView(deleteController, deleteViewModel);
 
         assertEquals("delete", deleteView.viewName);
